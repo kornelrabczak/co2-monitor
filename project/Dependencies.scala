10b390535663547eb41c91a3c3fe33dff95831a1
kornelrabczak <1546480+kornelrabczak@users.noreply.github.com>
2020-10-10 09:27:13 +0200

    MISC: Adds akka dependencies and build configuration

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,4 +2,16 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.2"
+
+  lazy val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
+
+  lazy val akkaVersion     = "2.6.8"
+  lazy val akkaHttpVersion = "10.2.0"
+
+  lazy val akka = Seq(
+    "com.typesafe.akka" %% "akka-actor" % akkaVersion % Compile,
+    "com.typesafe.akka" %% "akka-http" % akkaHttpVersion % Compile,
+    "com.typesafe.akka" %% "akka-testkit" % akkaVersion % Test,
+    "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion % Test
+  )
 }
