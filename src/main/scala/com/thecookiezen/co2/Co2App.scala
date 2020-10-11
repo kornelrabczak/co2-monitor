55c1cb9d1f0fa67b18bf29fc159044d536fdfb7b
kornelrabczak <1546480+kornelrabczak@users.noreply.github.com>
2020-10-11 09:35:37 +0200

    Adds acceptance tests to verify the whole flow

--- src/main/scala/com/thecookiezen/co2/Co2App.scala
+++ src/main/scala/com/thecookiezen/co2/Co2App.scala
@@ -2,35 +2,37 @@ package com.thecookiezen.co2
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Route
 import com.thecookiezen.co2.sensor.SensorCoordinator
 import com.thecookiezen.co2.web.SensorAPI
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 
+import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
 
-object Co2App extends App {
+object Co2App {
 
-  val config = ConfigFactory.load("sensor.conf");
+  private val config: Config = ConfigFactory.load("sensor.conf");
+  private val thresholdLevel = config.getInt("sensor.co2_threshold_level")
+  private val port = config.getInt("server.port")
 
-  val thresholdLevel = config.getInt("sensor.co2_threshold_level")
-  val port = config.getInt("server.port")
+  private implicit val system: ActorSystem = ActorSystem()
+  private implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+  private val sensorCoordinator = system.actorOf(SensorCoordinator.props(thresholdLevel))
 
-  implicit val system = ActorSystem()
-  implicit val executionContext = system.dispatcher
+  def main(args: Array[String]): Unit = {
+    val bindingFuture = Http().newServerAt("localhost", port).bind(routes)
 
-  val sensorCoordinator = system.actorOf(SensorCoordinator.props(thresholdLevel))
+    StdIn.readLine()
+    bindingFuture
+      .flatMap(_.unbind())
+      .onComplete(_ => system.terminate())
+  }
 
-  val bindingFuture = Http().newServerAt("localhost", port).bind(routes)
-
-  def routes = new SensorAPI(
+  def routes: Route = new SensorAPI(
     storeMeasurement = SensorCoordinator.storeMeasurement(sensorCoordinator),
     getStatus = SensorCoordinator.getStatus(sensorCoordinator),
     getStatistics = SensorCoordinator.getStatistics(sensorCoordinator),
     getAlerts = SensorCoordinator.getAlerts(sensorCoordinator)
   ).routes
-
-  StdIn.readLine()
-  bindingFuture
-    .flatMap(_.unbind())
-    .onComplete(_ => system.terminate())
 }
